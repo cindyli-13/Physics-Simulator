6842ad491eb9f50f9ac3d1b5de1fe1bc1107780b
Cindy Li <cindyli0213@hotmail.com>
2018-04-16 10:59:26 -0400

    Add files

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * This class contains the main method for the Physics Simulator 
+ * project.
+ * 
+ * @author Cindy Li, Larissa Jin
+ * @since Monday, April 16th, 2018
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
